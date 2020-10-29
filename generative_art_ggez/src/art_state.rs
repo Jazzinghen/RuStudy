8e267677a834c46a42e5bc59a497f5ac9e393363
Michele Bianchi <jazzinghen@gmail.com>
2020-10-29 22:29:36 +0900

    Every time I implement matches in Rust I am happy

--- generative_art_ggez/src/art_state.rs
+++ generative_art_ggez/src/art_state.rs
@@ -4,22 +4,44 @@ use ggez::mint;
 use ggez::Context;
 use ggez::GameResult;
 
-pub struct Art_State {}
+pub struct Art_State {
+    pub shapes: Vec<Shape>,
+}
+
+pub enum Shape {
+    Circle(mint::Point2<f32>, f32),
+    Rectangle(graphics::Rect),
+}
 
 impl event::EventHandler for Art_State {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            mint::Point2 { x: 200.0, y: 300.0 },
-            100.0,
-            0.1,
-            graphics::WHITE,
-        )?;
-        graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
+        for s in &self.shapes {
+            let new_shape = match s {
+                &Shape::Circle(centre, radius) => graphics::Mesh::new_circle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    centre,
+                    radius,
+                    0.1,
+                    graphics::WHITE,
+                )?,
+                &Shape::Rectangle(rect) => graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    rect,
+                    graphics::Color {
+                        r: 1.0,
+                        g: 0.0,
+                        b: 1.0,
+                        a: 1.0,
+                    },
+                )?,
+            };
+            graphics::draw(ctx, &new_shape, graphics::DrawParam::default())?;
+        }
         graphics::present(ctx)?;
         Ok(())
     }
--- generative_art_ggez/src/main.rs
+++ generative_art_ggez/src/main.rs
@@ -1,13 +1,22 @@
 use ggez::event;
+use ggez::mint;
 use ggez::ContextBuilder;
 
 mod art_state;
 
 fn main() {
+    let mut shape_vec = Vec::new();
+    shape_vec.push(art_state::Shape::Rectangle(ggez::graphics::Rect::new(
+        10.0, 10.0, 50.0, 100.0,
+    )));
+    shape_vec.push(art_state::Shape::Circle(
+        mint::Point2 { x: 400.0, y: 40.0 },
+        30.0,
+    ));
     // When doing it like this we are storing a constant reference to a mutable object
     // meaning that we _cannot_ change the memory location pointed by the variable, but
     // we can change the contents of the object pointed by it.
-    let state = &mut art_state::Art_State {};
+    let state = &mut art_state::Art_State { shapes: shape_vec };
 
     let context_builder = ContextBuilder::new("generative_art", "Jazzinghen");
     // Unwrap the value contained in the returned Enum... If correct?
