c7e46993699bfab43112c19cdb69b57d29e3d4b2
Michele Bianchi <jazzinghen@gmail.com>
2020-10-30 21:45:45 +0900

    I have learned some stuff :3

--- generative_art_ggez/src/art_state.rs
+++ generative_art_ggez/src/art_state.rs
@@ -1,11 +1,48 @@
 use ggez::event;
 use ggez::graphics;
 use ggez::mint;
+use ggez::timer;
 use ggez::Context;
 use ggez::GameResult;
+use rand::prelude::*;
 
-pub struct Art_State {
-    pub shapes: Vec<Shape>,
+pub struct ArtState {
+    art_refresh: std::time::Duration,
+    shapes: Vec<Shape>,
+    delta_t: std::time::Duration,
+    rng: ThreadRng,
+}
+
+impl ArtState {
+    pub fn new(art_refresh: std::time::Duration) -> ArtState {
+        ArtState {
+            art_refresh,
+            delta_t: std::time::Duration::new(0, 0),
+            shapes: Vec::new(),
+            rng: thread_rng(),
+        }
+    }
+
+    fn generate_shapes(&mut self, amount: u16) {
+        for _ in 1..amount {
+            if self.rng.gen() {
+                self.shapes.push(Shape::Rectangle(ggez::graphics::Rect::new(
+                    self.rng.gen_range(0.0, 800.0),
+                    self.rng.gen_range(0.0, 600.0),
+                    self.rng.gen_range(0.0, 800.0),
+                    self.rng.gen_range(0.0, 600.0),
+                )));
+            } else {
+                self.shapes.push(Shape::Circle(
+                    mint::Point2 {
+                        x: self.rng.gen_range(0.0, 800.0),
+                        y: self.rng.gen_range(0.0, 600.0),
+                    },
+                    self.rng.gen_range(0.0, 300.0),
+                ));
+            }
+        }
+    }
 }
 
 pub enum Shape {
@@ -13,11 +50,20 @@ pub enum Shape {
     Rectangle(graphics::Rect),
 }
 
-impl event::EventHandler for Art_State {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+impl event::EventHandler for ArtState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.delta_t += ggez::timer::delta(ctx);
+
+        if self.delta_t >= self.art_refresh {
+            self.shapes.clear();
+            self.generate_shapes(8);
+
+            self.delta_t = std::time::Duration::new(0, 0);
+        };
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, graphics::BLACK);
         for s in &self.shapes {
             let new_shape = match s {
                 &Shape::Circle(centre, radius) => graphics::Mesh::new_circle(
@@ -43,6 +89,8 @@ impl event::EventHandler for Art_State {
             graphics::draw(ctx, &new_shape, graphics::DrawParam::default())?;
         }
         graphics::present(ctx)?;
+
+        timer::yield_now();
         Ok(())
     }
 }
